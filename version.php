be399216c5626d3037d4b135a65af27b9401d3b8
Niranjan <eabyas@gmail.com>
2013-03-13 00:43:05 -0500

    Added the version file

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2012112900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component    = 'block_enrolltranscripts';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.x (Build: 2012061700)';
